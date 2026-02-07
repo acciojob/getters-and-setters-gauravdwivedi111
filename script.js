29476ad80c995fbe16a5bbf9acc8c47818c54aa0
gauravdwivedi111 <dwivedigaurav646@gmail.com>
2026-02-07 10:12:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-//complete this code
-class Person {}
+class Person {
+  #name;
+  #age;
 
-class Student extends Person {}
+  constructor(name, age) {
+    this.#name = name;
+    this.#age = age;
+  }
 
-class Teacher extends Person {}
+  // Getter for name
+  get name() {
+    return this.#name;
+  }
+
+  // Getter for age
+  get age() {
+    return this.#age;
+  }
+
+  // Setter for age
+  set age(year) {
+    if (year > 0) this.#age = year;
+    else console.log("Invalid age");
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(this.name + " is studying");
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(this.name + " is teaching");
+  }
+}
+
+// Testing
+let obj1 = new Person("John", 25);
+console.log(obj1.name); // John
+obj1.age = 30;
+console.log(obj1.age);  // 30
+
+let obj2 = new Student("Alice", 22);
+obj2.study();           // Alice is studying
+
+let obj3 = new Teacher("Bob", 40);
+obj3.teach();           // Bob is teaching
 
 // Do not change the code below this line
 window.Person = Person;
